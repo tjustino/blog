fb4e10c010acad2ceebee56fff3eafb14ef93684
tjustino <thomas.justino@gmail.com>
2013-07-31 16:34:52 +0200

    mise à jour capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,26 +9,29 @@ set :default_environment, {
   'PATH' => "$HOME/.rbenv/shims:$HOME/.rbenv/bin:$PATH"
 }
 
+# do not use sudo
 set :use_sudo, false
+set(:run_method) { use_sudo ? :sudo : :run }
 
+# This is needed to correctly handle sudo password prompt
 default_run_options[:pty] = true
 
 set :user, "tomj"
 set :group, user
 set :runner, user
 
-set :host, "#{user}@justino.fr"
+set :host, "#{user}@justino.fr" # We need to be able to SSH to that box as this user.
 role :web, host
 role :app, host
 
 set :rack_env, :production
 
+# Where will it be located on a server?
 set :deploy_to, "/home/tomj/www/#{application}"
-set :unicorn_conf, "#{current_path}/config/unicorn.rb"
-set :unicorn_pid, "#{shared_path}/pids/unicorn.pid"
+set :unicorn_conf, "#{deploy_to}/current/config/unicorn.rb"
+set :unicorn_pid, "#{deploy_to}/shared/pids/unicorn.pid"
 
-set :public_children, ["stylesheets","images"]
-
+# Unicorn control tasks
 namespace :deploy do
   task :restart do
     run "if [ -f #{unicorn_pid} ]; then kill -USR2 `cat #{unicorn_pid}`; else cd #{current_path} && bundle exec unicorn -c #{unicorn_conf} -E #{rack_env} -D; fi"
