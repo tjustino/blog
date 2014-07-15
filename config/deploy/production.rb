3d47179f1cc7c9ba25bd0c137bfae08144125040
tjustino <thomas.justino@gmail.com>
2014-07-15 17:09:59 +0200

    server name update for subdomain

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,7 @@
 set :stage,     :production
 set :branch,    "master"
 
-set :server_name, "www.justino.fr justino.fr"
+set :server_name, "www.justino.fr justino.fr thomas.justino.fr"
 set :enable_ssl,  false
 
 server "justino.fr", user: "tomj", roles: %w{web app}, primary: true
