42925b558701688d9519aa3c8096aa5f1c8a2ed0
tjustino <thomas.justino@gmail.com>
2014-10-06 16:02:23 +0200

    refactoring

--- lib/app.rb
+++ lib/app.rb
@@ -3,16 +3,22 @@ require "rdiscount"
 
 set :root, File.expand_path('../../', __FILE__)
 
-get "/" do
-  file = File.read("#{settings.root}/views/pitch.md")
-  pitch = RDiscount.new(file)
+def read_markdown(file)
+  markdown = File.read("#{settings.root}/views/#{file}")
+  pitch = RDiscount.new(markdown)
 
-  erb :index, locals: { pitch: pitch.to_html }
+  pitch.to_html
 end
 
-# get "/phrases-qui-marquent" do
-#   slim :phrases_qui_marquent
-# end
+get "/" do
+  erb :index, locals: { pitch: read_markdown("root.md") }
+end
+
+["/kdo", "/cadeau", "/cadeaux"].each do |path|
+  get path do
+    erb :index, locals: { pitch: read_markdown("kdo.md") }
+  end
+end
 
 get "/*" do
   redirect "/"
