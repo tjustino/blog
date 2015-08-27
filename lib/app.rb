f9b82f279fd223cc4fa872b3619c8da5ccb110e3
tjustino <thomas.justino@gmail.com>
2015-08-27 12:32:01 +0200

    removing rdiscount

--- lib/app.rb
+++ lib/app.rb
@@ -1,22 +1,14 @@
 require "sinatra"
-require "rdiscount"
 
 set :root, File.expand_path('../../', __FILE__)
 
-def read_markdown(file)
-  markdown = File.read("#{settings.root}/views/#{file}")
-  pitch = RDiscount.new(markdown)
-
-  pitch.to_html
-end
-
 get "/" do
-  erb :index, locals: { pitch: read_markdown("root.md") }
+  slim :index, locals: { pitch: :main }
 end
 
 ["/kdo", "/cadeau", "/cadeaux"].each do |path|
   get path do
-    erb :index, locals: { pitch: read_markdown("kdo.md") }
+    slim :index, locals: { pitch: :kdo }
   end
 end
 
