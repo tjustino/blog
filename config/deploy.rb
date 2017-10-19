dcd7025253e14d25ac99c29d1ae92dd5af52d084
tjustino <thomas.justino@gmail.com>
2017-10-19 14:03:45 +0200

    update to ruby 2.4.2

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,7 +12,7 @@ set :ssh_options, { forward_agent: true }
 
 # setup rbenv
 set :rbenv_type,      :user
-set :rbenv_ruby,      "2.4.1"
+set :rbenv_ruby,      "2.4.2"
 set :rbenv_prefix,    "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins,  %w{rake gem bundle ruby puma pumactl}
 
