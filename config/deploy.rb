f35d80e1de67d6a3b801106e6fb3a450329e1d19
tjustino <thomas.justino@gmail.com>
2022-06-29 22:52:51 +0200

    mina deployment enhancement

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,25 +11,18 @@ set :deploy_to,        "/srv/http/#{fetch(:application_name)}"
 set :repository,       "git@github.com:tjustino/blog.git"
 set :branch,           "main"
 set :bundle_path,      "vendor/bundle"
+set :forward_agent,    true
+set :bundle_options,   -> { '' }
 
-# Optional settings:
-#   set :user, "foobar"          # Username in the server to SSH to.
-#   set :port, "30000"           # SSH port number.
-#   set :forward_agent, true     # SSH forward_agent.
-
-# Shared dirs and files will be symlinked into the app-folder by the "deploy:link_shared_paths" step.
-# Some plugins already add folders to shared_dirs like `mina/rails` add `public/assets`, `vendor/bundle` and many more
-# run `mina -d` to see all folders and files already included in `shared_dirs` and `shared_files`
-# set :shared_dirs, fetch(:shared_dirs, []).push("public/assets")
 set :shared_dirs, fetch(:shared_dirs, []).push("log", "tmp/cache", "tmp/sockets", fetch(:bundle_path))
-# set :shared_files, fetch(:shared_files, []).push("config/database.yml", "config/secrets.yml")
 
-# This task is the environment that is loaded for all remote run commands, such as
-# `mina deploy` or `mina rake`.
 task :remote_environment do
   # If you're using rbenv, use this to load the rbenv environment.
   # Be sure to commit your .ruby-version or .rbenv-version to your repository.
   invoke :"rbenv:load"
+  command "#{fetch(:bundle_bin)} config set deployment 'true'"
+  command "#{fetch(:bundle_bin)} config set path '#{fetch(:bundle_path)}'"
+  command "#{fetch(:bundle_bin)} config set without '#{fetch(:bundle_withouts)}'"
 end
 
 # Put any custom commands you need to run at setup
@@ -43,7 +36,7 @@ end
 desc "Deploys the current version to the server."
 task :deploy do
   # uncomment this line to make sure you pushed your local branch to the remote origin
-  # invoke :"git:ensure_pushed"
+  invoke :"git:ensure_pushed"
   deploy do
     # Put things that will set up an empty directory into a fully set-up
     # instance of your project.
@@ -60,5 +53,5 @@ task :deploy do
     end
   end
 
-  run(:local) { say "done 🎉" }
+  run(:local) { puts "done 🎉" }
 end
