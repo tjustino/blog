c61a454190d51428babcaf7f6ca1e0bb0c16f53f
tjustino <thomas.justino@gmail.com>
2023-07-20 15:29:17 +0200

    DRY

--- config/puma/production.rb
+++ config/puma/production.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
-directory "/srv/http/blog/current"
+root_deploiement = File.expand_path("../../../../", File.dirname(__FILE__))
+
+directory "#{root_deploiement}/current"
 environment "production"
 
-pidfile "/srv/http/blog/shared/tmp/pids/puma.pid"
-state_path "/srv/http/blog/shared/tmp/pids/puma.state"
-stdout_redirect "/srv/http/blog/shared/log/puma_access.log", "/srv/http/blog/shared/log/puma_error.log", true
+pidfile "#{root_deploiement}/shared/tmp/pids/puma.pid"
+state_path "#{root_deploiement}/shared/tmp/pids/puma.state"
+stdout_redirect "#{root_deploiement}/shared/log/puma_access.log", "#{root_deploiement}/shared/log/puma_error.log", true
 
-bind "unix:///srv/http/blog/shared/tmp/sockets/puma.sock"
+bind "unix://#{root_deploiement}/shared/tmp/sockets/puma.sock"
