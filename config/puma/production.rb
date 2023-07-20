297219996b7831dcbef2ced22e321977682705c8
tjustino <thomas.justino@gmail.com>
2023-07-20 16:19:20 +0200

    must be asbolute path

--- config/puma/production.rb
+++ config/puma/production.rb
@@ -1,12 +1,10 @@
 # frozen_string_literal: true
 
-root_deploiement = File.expand_path("../../../", File.dirname(__FILE__))
-
-directory "#{root_deploiement}/current"
+directory "/home/tomj/www/blog/current"
 environment "production"
 
-pidfile "#{root_deploiement}/shared/tmp/pids/puma.pid"
-state_path "#{root_deploiement}/shared/tmp/pids/puma.state"
-stdout_redirect "#{root_deploiement}/shared/log/puma_access.log", "#{root_deploiement}/shared/log/puma_error.log", true
+pidfile "/home/tomj/www/blog/shared/tmp/pids/puma.pid"
+state_path "/home/tomj/www/blog/shared/tmp/pids/puma.state"
+stdout_redirect "/home/tomj/www/blog/shared/log/puma_access.log", "/home/tomj/www/blog/shared/log/puma_error.log", true
 
-bind "unix://#{root_deploiement}/shared/tmp/sockets/puma.sock"
+bind "unix:///home/tomj/www/blog/shared/tmp/sockets/puma.sock"
