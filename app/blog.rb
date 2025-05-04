bd5649e846d9e26e07cae328a58c9b51571c0270
tjustino <thomas@justino.fr>
2025-05-04 12:22:50 +0200

    new page for tool words

--- app/blog.rb
+++ app/blog.rb
@@ -20,6 +20,20 @@ class MyBlog < Sinatra::Base
     end
   end
 
+  ["/mots-outils", "/mots", "/mo"].each do |path|
+    get path do
+      @set1 = ["un", "le", "il y a ", "sur", "dans", "la", "et", "sous", "est",
+               "une", "il", "aussi", "des", "c'est", "elle", "les", "du"].sample
+      @set2 = ["avec", "ses", "j'ai", "même", "mon", "pour", "de", "mes", "près de", "mais",
+               "car", "son", "comme", "non", "il fait", "autour", "puis", "déjà", "chez"].sample
+      @set3 = %w[quand tes cette aussitôt mais toujours qui
+                 debout ensuite devant derrière après voici celle].sample
+      @set4 = ["voilà", "encore", "alors", "soudain", "c'était", "vers",
+               "peut-être", "dedans", "tout", "quel", "chacun"].sample
+      erb :mots
+    end
+  end
+
   get "/up" do
     "<!DOCTYPE html><html><body style=\"background-color: green\"></body></html>"
   end
