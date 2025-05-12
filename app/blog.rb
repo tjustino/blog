56c9081b842ca1b302de01ac055e933465d056d3
tjustino <thomas@justino.fr>
2025-05-12 16:32:16 +0200

    fixing some offense

--- app/blog.rb
+++ app/blog.rb
@@ -22,14 +22,21 @@ class MyBlog < Sinatra::Base
 
   ["/mots-outils", "/mots", "/mo"].each do |path|
     get path do
+      # Année scolaire 2025-2026
+      # https://www.education.gouv.fr/bo/2024/Hebdo41/MENE2415135A
+      # Français      → https://www.education.gouv.fr/sites/default/files/ensel135_annexe3.pdf
+      # Mathématiques → https://www.education.gouv.fr/sites/default/files/ensel135_annexe4.pdf
+
+      # https://media.education.gouv.fr/file/02_fevrier/24/3/BOEcolePrimaireWeb_24243.pdf
       @mots_outils = ["à", "alors", "après", "au", "aussi", "aussitôt", "autour", "avant", "avec", "bien", "c'est",
-        "c'était", "car", "ce", "celle", "ces", "cette", "chacun", "chez", "comme", "comment", "dans", "de", "debout",
-        "dedans", "derrière", "des", "devant", "dont", "du", "déjà", "elle", "elles", "en", "encore", "ensuite",
-        "entre", "est", "et", "ici", "il", "il fait", "il y a", "ils", "j'ai", "je", "l'", "la", "le", "les", "ma",
-        "mais", "mes", "moins", "mon", "même", "ne … jamais", "ne … pas", "ne … plus", "non", "nous", "on", "ou", "oui",
-        "où", "par", "peut-être", "plus", "pour", "près", "près de", "puis", "quand", "que", "quel", "quelle", "qui",
-        "quoi", "sa", "sans", "ses", "si", "son", "soudain", "sous", "sur", "ta", "tard", "tes", "ton", "toujours",
-        "tout", "trop", "très", "tu", "tôt", "un", "une", "vers", "voici", "voilà", "vous", "y"].sample(8)
+                      "c'était", "car", "ce", "celle", "ces", "cette", "chacun", "chez", "comme", "comment", "dans",
+                      "de", "debout", "dedans", "derrière", "des", "devant", "dont", "du", "déjà", "elle", "elles",
+                      "en", "encore", "ensuite", "entre", "est", "et", "ici", "il", "il fait", "il y a", "ils", "j'ai",
+                      "je", "l'", "la", "le", "les", "ma", "mais", "mes", "moins", "mon", "même", "ne … jamais",
+                      "ne … pas", "ne … plus", "non", "nous", "on", "ou", "oui", "où", "par", "peut-être", "plus",
+                      "pour", "près", "près de", "puis", "quand", "que", "quel", "quelle", "qui", "quoi", "sa", "sans",
+                      "ses", "si", "son", "soudain", "sous", "sur", "ta", "tard", "tes", "ton", "toujours", "tout",
+                      "trop", "très", "tu", "tôt", "un", "une", "vers", "voici", "voilà", "vous", "y"].sample(8)
 
       erb :mots
     end
